016d2dbd3abdf727618cf8b89a0793574834cff4
muhamad-arido <mhd.arido@gmail.com>
2024-07-09 23:59:30 +0700

    feat: create MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule],
   controllers: [],
   providers: [],
 })
